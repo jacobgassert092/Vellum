ab2762cecf5c7c104fa8865cec908e73c37fff09
Jacob Gassert <jacobgassert092@gmail.com>
2026-03-13 17:15:09 -0400

    Github Pages Test

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_VALUE === 'production';
+const repoName = "Vellum"
+
 const nextConfig = {
   output: 'export', // For GitHub Pages
   images: {
     unoptimized: true, // Necessary for static exports
   },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
 };
 
 export default nextConfig;
